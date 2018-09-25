b730da5057ccb2262f06d5c2917f4a6030025a74
ning <1991407206@qq.com>
2018-09-25 18:51:35 +0800

    mock

--- mock3/server.js
+++ mock3/server.js
@@ -5,10 +5,19 @@ const mime = require('./mime')
 
 const processRequest = (request, response) => {
   let pathName = url.parse(request.url).pathname
+  // 防止中文乱码
+  pathName = decodeURI(pathName)
   // 获取资源文件的绝对路径
   let filePath = path.resolve(__dirname + '/mockapi/' + pathName)
 
+  // 301重定向，目录需以 `/` 结尾
   if (!pathName.endsWith('/') && path.extname(pathName) === '') {
+    pathName += '/'
+    var redirect = 'http://' + request.headers.host + pathName
+    response.writeHead(301, {
+      location: redirect
+    })
+
     response.end()
   }
 
@@ -19,14 +28,49 @@ const processRequest = (request, response) => {
   let contentType = mime[ext] || "text/plain"
 
   fs.stat(filePath, (err, stats) => {
+    console.log('err::', err)
+    console.log('stats::', stats)
     if (err) {
       response.writeHead(404, {"content-type": contentType})
       response.end("<h1>404 Not Found</h1>")
     }
 
-    if (stats.isFile()) {
-      response.writeHead(200, { "content-type": contentType });
-      response.end("<h1>Success</h1>")
+    // 文件
+    if (!err && stats.isFile()) {
+      response.writeHead(200, {"content-type": contentType});
+      // 读文件
+      var stream = fs.createReadStream(filePath)
+
+      // 错误处理
+      stream.on('error', () => {
+        response.writeHead(500, {"content-type": contentType})
+        response.end('<h1>500 Server Error</h1>')
+      })
+
+      stream.pipe(response)
+      response.end()
+    }
+
+    // 目录
+    if (!err && stats.isDirectory()) {
+      var html = '<head><meta charset="utf-8" /></head>'
+      fs.readdir(filePath, (err, files) => {
+        if (err) {
+          console.log("读取路径失败！")
+        } else {
+          for (var file of files) {
+            if (file === 'index.html') {
+              response.writeHead(200, {"content-type": "text/html"})
+              response.end(file)
+              break
+            }
+
+            html += `<div><a href="${file}">${file}</a></div>`
+            response.writeHead(200, {"content-type": "text/html"})
+            response.end(html)
+          }
+        }
+      })
     }
   })
 }
